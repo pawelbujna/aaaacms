2214b78b1286e512ca79d38f1f4165ff94634b75
Paweł Bujna <pawelbujna@gmail.com>
2020-11-27 17:33:15 +0100

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f682b5b9a25be07b4ca084835bb51602'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f68f9af5-9914-4189-9429-19e8d80b3f2c'
+};
